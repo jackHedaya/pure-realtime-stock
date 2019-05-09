871d1e0ded4ee3a6a443960652290370154dfeef
Jack Hedaya <jackehedaya@gmail.com>
2019-05-08 23:05:22 -0400

    clarified message

--- preinstall.js
+++ preinstall.js
@@ -12,6 +12,6 @@ if (process.platform === "linux") {
       chalk.bold.white("sh node_modules/pure-realtime-stock/deps.sh\n")
   );
   
-  console.log(chalk.yellow("If there is an error during installation, try again with "))
+  console.log(chalk.yellow("If there is an error during installation, delete node_modules and try again with "))
   console.log(chalk.bold.white("sudo npm i pure-realtime-stock --unsafe-perm=true"))
 }
