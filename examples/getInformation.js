4c2ae7904e5eeaac2417ca176617cc33917aad14
Jack Hedaya <jackehedaya@gmail.com>
2019-05-08 22:57:21 -0400

    close realtime in example

--- examples/getInformation.js
+++ examples/getInformation.js
@@ -2,5 +2,9 @@ const RealtimeStock = require("../index");
 
 const realtime = new RealtimeStock();
 
-realtime.getInformation("pcg").then(info => console.log(info))
-realtime.on("debug", (x) => console.log(x))
+realtime.getInformation("pcg").then(info => {
+  console.log(info);
+  realtime.close();
+});
+
+realtime.on("debug", x => console.log(x));
